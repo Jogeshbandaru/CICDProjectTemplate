6301dd10dbaf60cbba268b24bb576979792e275a
Jogesh <jogesh.bandaru@gmail.com>
2025-02-22 20:17:35 -0600

    updated terraform.sh file

--- ec2.tf
+++ ec2.tf
@@ -5,5 +5,5 @@ provider "aws" {
 resource "aws_instance" "demo_server" {
     ami = "ami-05b10e08d247fb927"
     instance_type = "t2.micro"
-    key_name = "jogesh-kp"
+    key_name = "jogesh-kp1"
 }
